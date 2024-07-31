17092d8e73c0349b48b00df0505b2b721442f1c2
Jonathan Treffler <mail@jonathan-treffler.de>
2024-07-31 09:37:01 +0000

    init gitpod

---
+++ gitpod/nextcloud/custom-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Both gitpod and Nextcloud need read/write acess
+# Obviously not recommended for a production system
+chown -R www-data:www-data /var/www/html/custom_apps/
+chmod -R 777 /var/www/html/custom_apps/
+
+/entrypoint.sh "$@"
